23bf0d257635c963e12cf4261ded46bd0cf1efe8
conansgold@gmail.com <conansgold@gmail.com>
2019-06-03 21:33:22 -0500

    Created basic file saving in Grades Activity. Need to make more modular and do further testing. Next step add a function to create new grades.

--- app/src/main/java/com/goldengears/gradeestimator/GradesActivity.java
+++ app/src/main/java/com/goldengears/gradeestimator/GradesActivity.java
@@ -10,7 +10,15 @@
 
  import com.goldengears.gradeestimator.adapters.GradesRecyclerAdapter;
  import com.goldengears.gradeestimator.models.Grade;
+ import com.google.gson.Gson;
+ import com.google.gson.reflect.TypeToken;
 
+ import java.io.BufferedReader;
+ import java.io.FileNotFoundException;
+ import java.io.IOException;
+ import java.io.InputStream;
+ import java.io.InputStreamReader;
+ import java.io.OutputStreamWriter;
  import java.sql.Time;
  import java.util.ArrayList;
  import java.util.Date;
@@ -35,8 +43,8 @@
      }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_list);
         mRecyclerView = findViewById(R.id.recyclerView);
@@ -45,8 +53,26 @@
 
         mGradesActivityContext = this;
 
-        Random rand = new Random();
-        insertFakeGrade(rand.nextInt(1000));
+         //Get the info from the file and if it is empty create test data
+         String fileInfo = readFromFile(getmGradesActivityContext());
+         if (fileInfo == null || fileInfo.length() == 0 || fileInfo.trim().length() == 0) {
+             Random rand = new Random();
+             insertFakeGrade(rand.nextInt(10));
+
+             Gson gson = new Gson();
+             String arrayData = gson.toJson(mGrades);
+             writeToFile(arrayData, getmGradesActivityContext());
+             Log.d(TAG, "onCreate: written to file " + arrayData);
+         } else {
+             Gson data = new Gson();
+             mGrades.addAll(data.fromJson(fileInfo, new TypeToken<ArrayList<Grade>>() {
+             }.getType()));
+
+             Log.d(TAG, "onCreate: " + data);
+             Log.d(TAG, "onCreate: " + mGrades);
+             Random rand = new Random();
+         }
+
 
     }
 
@@ -70,4 +96,46 @@
          mRecyclerView.setAdapter(mGradesRecyclerAdapter);
      }
 
-}
+     private String readFromFile(Context context) {
+
+         String ret = "";
+         Log.d(TAG, "readFromFile: reading from file");
+
+         try {
+             InputStream inputStream = context.openFileInput("config.txt");
+
+             if (inputStream != null) {
+                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                 String receiveString = "";
+                 StringBuilder stringBuilder = new StringBuilder();
+
+                 while ((receiveString = bufferedReader.readLine()) != null) {
+                     stringBuilder.append(receiveString);
+                 }
+
+                 inputStream.close();
+                 ret = stringBuilder.toString();
+             }
+         } catch (FileNotFoundException e) {
+             Log.e("login activity", "File not found: " + e.toString());
+         } catch (IOException e) {
+             Log.e("login activity", "Can not read file: " + e.toString());
+         }
+
+         Log.d(TAG, "readFromFile: return is empty? " + ret.isEmpty());
+         Log.d(TAG, "readFromFile: " + ret);
+         return ret;
+     }
+
+     private void writeToFile(String data, Context context) {
+         try {
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+             outputStreamWriter.write(data);
+             outputStreamWriter.close();
+         } catch (IOException e) {
+             Log.e("Exception", "File write failed: " + e.toString());
+         }
+     }
+
+ }
